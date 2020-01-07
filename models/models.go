235b91db60603d79db6798bc6dbf945cadda2c8d
2wangjingjun8 <18813967628@163.com>
2020-01-07 23:16:21 +0800

    0107

--- models/models.go
+++ models/models.go
@@ -1,6 +1,8 @@
 package models
 
 import (
+	"time"
+
 	"github.com/astaxie/beego/orm"
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -12,8 +14,18 @@ type User struct {
 	Password string
 }
 
+// Article 文章表
+type Article struct {
+	ID       int    `orm:"pk;auto"`
+	Artiname string `orm:"size(20)"`
+	Atime    time.Time
+	Acount   int `orm:"default(20);null"`
+	Acontent string
+	Aimg     string
+}
+
 func init() {
 	orm.RegisterDataBase("default", "mysql", "root:root@tcp(127.0.0.1:3306)/test_beego?charset=utf8")
-	orm.RegisterModel(new(User))
+	orm.RegisterModel(new(User), new(Article))
 	orm.RunSyncdb("default", false, true)
 }
--- routers/router.go
+++ routers/router.go
@@ -11,4 +11,7 @@ func init() {
 	beego.Router("/register", &controllers.MainController{})
 	beego.Router("/login", &controllers.MainController{}, "get:ShowLogin;post:HandleLogin")
 	beego.Router("/index", &controllers.MainController{}, "get:ShowIndex;post:HandleIndex")
+	beego.Router("/article_list", &controllers.MainController{}, "get:ShowArticle")
+	beego.Router("/article_add", &controllers.MainController{}, "get:ShowAddArticle;post:HandleAddArticle")
+	beego.Router("/article_edit", &controllers.MainController{}, "get:ShowEditArticle;post:HandleEditArticle")
 }
